b24dfb72fedb48a2364dfd04228b700513867b8e
Manuel Antonio Neto Madeira Amado <maba@student.dei.uc.pt>
2017-04-21 22:20:22 +0100

    caso de teste 3-StringArray.java

---
+++ TestFiles/3-StringArray.java
@@ -0,0 +1,24 @@
+class stringArrays {
+
+	public static int  main (String[] args){
+		int a;
+		boolean b;
+		double c;
+        return 1;
+	}
+	public static boolean bool(String[] a){
+	return false;
+	}
+	public static boolean bool(String[] a){
+	return false;
+	}
+	public static boolean bool(String[] a){
+	return false;
+	}
+	public static boolean bool(String[] a){
+	return false;
+	}
+	public static boolean bool(String[] b){
+	return false;
+	}
+}
